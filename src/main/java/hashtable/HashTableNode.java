992bb01dee6434687045e169d563cc1c2ed15b46
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-08-16 16:45:23 +0530

    Added: Find Frequency of Words from a given sentence

--- src/main/java/hashtable/HashTableNode.java
+++ src/main/java/hashtable/HashTableNode.java
@@ -1,2 +1,48 @@
-package hashtable;public class HashTableNode {
+package hashtable;
+
+public class HashTableNode<K , V> {
+
+    K key;
+    V value;
+    HashTableNode<K, V> next;
+
+    public HashTableNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+        next = null;
+
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public HashTableNode<K, V> getNext() {
+        return next;
+    }
+
+    public void setNext(HashTableNode<K, V> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder nodeString = new StringBuilder();
+        nodeString.append("Node{" + "Key=").append(key).append(" Value=").append(value).append("}");
+        if(next != null)
+            nodeString.append("->").append(next);
+        return nodeString.toString();
+    }
 }
--- src/main/java/hashtable/HashTableRunner.java
+++ src/main/java/hashtable/HashTableRunner.java
@@ -1,2 +1,28 @@
-package hashtable;public class HashTableRunner {
+package hashtable;
+
+public class HashTableRunner {
+    public static void main(String[] args) {
+
+        HashTableOperation<String, Integer> hashTable = new HashTableOperation<String, Integer>();
+        String sentence = " you change your thought pattern. " +
+                "Once you change your thought pattern, you change your attitude. " +
+                "Once you change your attitude";
+
+        // Storing given sentence in an array.
+        // Converting the sentence to lower case and removing spaces.
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+        // Iterating over the array.
+        for (String word : sentenceArray) {
+            Integer count = hashTable.get(word);
+
+            if (count == null)
+                count = 1;
+            else
+                count = count + 1;
+            hashTable.add(word, count);
+        }
+        System.out.println(hashTable);
+
+    }
 }
